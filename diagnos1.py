c1033a39c0074eb2b9451c575420a5acedb910d1
Mattias Lindhe <mattias.lindhe@gmail.com>
2023-09-12 14:30:00 +0200

    Add Main file

---
+++ diagnos1.py
@@ -0,0 +1,14 @@
+#Diagnos: Skriv en funktion där utdata är alla printal mellan 2 och n. Där tal n är ett heltal mellan 1-99.
+#tips: Erathostenes såll.(se www.youtube.com/watch?v=qayM-pdHIU0 ) Publicera din lösning i repositoriet inlam som du delat med användaren AIDEV23S på gethub senast 230912 kl23:00
+
+#läs kap 3-8 i boken "Python från början" 
+
+#Gör minst 3 uppgifter ur kape3-8 i boken "Python från början"
+
+
+#input for number N (total number)
+#Generate all numbers
+#We need to compare number (proccess)
+#Out put prime numbers
+#
+#
